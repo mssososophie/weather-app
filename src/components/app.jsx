715433ab25f370afe86ffb8f2d459839445df46b
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2018-05-07 20:07:43 +0100

    adding location-details, step 5

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,6 @@
 import React from 'react';
+import LocationDetails from './location-details';
 
-import PropTypes from 'prop-types';
-
-const App = props => <h1>{`Hello ${props.name} you are ${props.age}!`}</h1>;
-
-App.propTypes = {
-  name: PropTypes.string.isRequired,
-};
-
-App.propTypes = {
-  age: PropTypes.number.isRequired,
-};
+const App = () => <LocationDetails city={props.location.city} country={props.location.country}  />
 
 export default App;
