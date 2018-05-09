6390da2a1313e4658c78116322b366179f27ef17
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2018-05-09 19:53:19 +0100

    restructuring tests for forecast elements

--- __tests__/components/forecast-summary.test.jsx
+++ __tests__/components/forecast-summary.test.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Enzyme from 'enzyme';
 import ForecastSummary from '../../src/components/forecast-summary';
 
-it('renders the passed date, temperature, description and icon in a p tag', () => {
+it('renders the passed datein a p tag', () => {
   const wrapper = Enzyme.shallow((
-    <ForecastSummary date="foo" temperature="bar" description="woo" icon="???" />
+    <ForecastSummary
+      date="mockDate"
+      temperature="mockTemperature"
+      description="mockDescription"
+      icon="mockIcon"
+    />
   ));
 
-  const text = wrapper.find('p').text();
-
-  expect(text).toEqual('foo, bar, woo, ???');
+  expect(wrapper.find(.'forecastsummary__date')).toEqual('mockDate');
 });
